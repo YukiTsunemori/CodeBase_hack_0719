8b4f4ed58f9a4db8fd00db674d6d78b15271e261
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-07-11 20:53:17 +0900

    新規登録エラ後のレンダリングでレイアウト崩れを修正

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Posting
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
 
     # Configuration for the application, engines, and railties goes here.
     #
