02302eca0127e5bd2d49642ad131a0ed33232c5d
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-07-12 12:41:43 +0900

    ログイン後の遷移先をroot_pathに明示的に設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :email ])
     end
+
+    def after_sign_in_path_for(resource_or_scope)
+      root_path  # ここを好きなパスに変更
+    end
 end
