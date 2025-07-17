96e1449b41334eb9cf226a42e1cec203851a3eb1
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-07-18 00:57:35 +0900

    投稿の作成日を降順で一覧取得するよう設定済み。

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :replies, dependent: :destroy
-  scope :ordered, -> { order(id: :desc) }
+  scope :ordered, -> { order(create_at: :desc) } # 投稿は作成日を降順に並べます。
   validates :text, presence: true, length: { maximum: 140 }
 end
