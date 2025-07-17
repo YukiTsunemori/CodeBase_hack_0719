86154608c4fd4d68e73d6ecd41cccd72cc496832
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-07-18 01:57:44 +0900

    タイポ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-  has_many :post
+  has_many :post, dependent: :destroy
+  has_many :replies, dependent: :destroy
 end
