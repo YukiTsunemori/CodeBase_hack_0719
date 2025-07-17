20884e92aa71b5e9b5944cb88e9e7525f335fe7b
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-07-18 00:58:36 +0900

    返信文は作成日を昇順で一覧取得するよう設定済み。

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,7 +1,7 @@
 class Reply < ApplicationRecord
   belongs_to :post, dependent: :destroy
   belongs_to :user, dependent: :destroy
-  scope :ordered, -> { order(id: :desc) }
+  scope :reply_ordered, -> { order(created_at: :asc) } # 返信文は作成日を昇順に並べます
   validates :text, presence: true, length: { maximum: 140 }
 
   # Additional methods or validations can be added here if neededc
