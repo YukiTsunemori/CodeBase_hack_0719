de34672e3f319b49db7499d5e58dea3cca8fd249
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-07-18 00:56:27 +0900

    createアクション失敗時にエラーが出ていたので@postsを再取得する記述をしました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,13 +11,19 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id]) # 投稿のidをもとに投稿を選択
-    @replies = Reply.joins(:user).select("replies.id, replies,text, replies.created_at, users.name as reply_user").ordered # 投稿に関連づけられた返信を一覧で取得
+    @replies = Reply.joins(:user).select("replies.id, replies,text, replies.created_at, users.name as reply_user").reply_ordered
     @reply = @post.replies.build # 投稿に関連づけられたオブジェクトを生成
   end
 
   def create
-    @post = Post.new(post_params)
-    @posts = Post.ordered
+     # 現在のユーザーに関連付けて投稿をビルドします。
+    # これにより、post.user_id が自動的に current_user.id に設定されます。
+    @post = current_user.post.build(post_params)
+
+    # 投稿の保存に失敗した場合に備え、@posts を再取得します。
+    # これにより、render :index が呼び出されたときに、
+    # 既存の投稿が正しく表示され、post_user エイリアスが利用可能になります。
+    @posts = Post.joins(:user).select("posts.id, posts.text, posts.created_at, users.name as post_user").ordered
     # binding.irb
     if @post.save
       redirect_to root_path, notice: "Post was successfully created."
