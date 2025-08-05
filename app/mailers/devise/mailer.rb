b888a883e4bc861433b653e8cf051cc21d25cd8e
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-08-05 20:56:44 +0900

    修正

--- app/mailers/devise/mailer.rb
+++ app/mailers/devise/mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-if defined?(ActionMailer)
-  class Devise::Mailer < Devise.parent_mailer.constantize
+module Devise
+  class Mailer < Devise.parent_mailer.constantize
     include Devise::Mailers::Helpers
 
     def confirmation_instructions(record, token, opts = {})
