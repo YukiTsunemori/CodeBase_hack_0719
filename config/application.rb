f8867b28fde3fe6b273a335322af7a4faa0bc993
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-08-05 17:50:30 +0900

    rails/allを削除し、必要なものだけをリスト

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,19 @@
 require_relative "boot"
 
-require "rails/all"
+# require "rails/all"
+require "rails"
+# Active Record (データベース)
+require "active_record/railtie"
+# Action Controller (コントローラー)
+require "action_controller/railtie"
+# Action View (ビュー)
+require "action_view/railtie"
+# Active Job (バックグラウンドジョブ)
+require "active_job/railtie"
+# Active Storage (ファイルアップロード)
+# require "active_storage/engine" # ← もし使わないならこれもコメントアウト
+# Action Cable (リアルタイム通信)
+require "action_cable/engine"
 Bundler.require(*Rails.groups)
 
 module Posting
